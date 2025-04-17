9b0d200aa1e7eac28360d5fff6f44153927e99b3
Rodrigo Fontes <rodrifontes@gmail.com>
2025-04-17 11:46:04 -0300

    Aula 01 - Create Projetct, Component Header

---
+++ src/Main/index.js
@@ -0,0 +1,13 @@
+import { Text } from "react-native";
+
+import { Container } from "./styles";
+
+import Header from '../components/Header';
+
+export default function Main() {
+  return (
+    <Container>
+      <Header />
+    </Container>
+  );
+}
